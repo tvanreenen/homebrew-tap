4b271b2f6bc24928791669cf8e799d1e88259683
Tim VanReenen <tim.vanreenen@gmail.com>
2026-03-31 19:23:07 -0400

    Update key cask to v0.1.1

--- Casks/key.rb
+++ Casks/key.rb
@@ -1,8 +1,8 @@
 cask "key" do
-  version "v0.1.0"
-  sha256 "8cd87cae22f22ddbdb44a01f194d93a0d7caa0493ca9f6baffb36d5a882b85f1"
+  version "v0.1.1"
+  sha256 "6d12236ea21e3a3933d5f75f3d8a57040868bc969a3ff8caba5f27f22367f898"
 
-  url "https://github.com/tvanreenen/key/releases/download/v0.1.0/Key-v0.1.0.zip"
+  url "https://github.com/tvanreenen/key/releases/download/v0.1.1/Key-v0.1.1.zip"
   name "key"
   desc "macOS file-based secret manager with native auth"
   homepage "https://github.com/tvanreenen/key"
